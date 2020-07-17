2c1eb72eb05e8e4a22d67fd660037826e6a130ed
Ananac0 <67715219+Ananac0@users.noreply.github.com>
2020-07-17 13:13:25 +0300

    Add files via upload

--- notescript.sh
+++ notescript.sh
@@ -49,8 +49,8 @@ apt update
 apt upgrade
 apt full-upgrade
 
-sudo for i in $(pip2 list -o | awk 'NR > 3 {print $1}'); do sudo pip2 install -U $i; done
-sudo for i in $(pip3 list -o | awk 'NR > 2 {print $1}'); do sudo pip3 install -U $i; done
+for i in $(pip2 list -o | awk 'NR > 3 {print $1}'); do sudo pip2 install -U $i; done
+for i in $(pip3 list -o | awk 'NR > 2 {print $1}'); do sudo pip3 install -U $i; done
 
 apt update
 apt upgrade
