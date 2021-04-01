7ef70a23d35c2454ecccc7c165e432ef1cf42419
Ananac0 <67715219+Ananac0@users.noreply.github.com>
2021-04-01 12:00:18 +0300

    Add files via upload

--- PreConf.sh
+++ PreConf.sh
@@ -33,6 +33,12 @@ echo -e "\e[32mapt auto-removing...\e[0m"
 sudo apt autoremove -y
 #apt installing 2 end
 
+echo -e "\e[32mapt installing leafpad...\e[0m"
+sudo apt install leafpad -y
+
+echo -e "\e[32mapt installing gufw...\e[0m"
+sudo apt install gufw -y
+
 echo -e "\e[32mapt installing tor...\e[0m"
 sudo apt install tor -y
 
@@ -52,6 +58,12 @@ git clone https://github.com/FluxionNetwork/fluxion
 
 echo -e "\e[32mGit cloning Airgeddon...\e[0m"
 git clone https://github.com/v1s1t0r1sh3r3/airgeddon
+
+echo -e "\e[32mGit cloning HiddenEye-Legacy...\e[0m"
+git clone https://github.com/DarkSecDevelopers/HiddenEye-Legacy
+
+sudo leafpad /etc/systemd/logind.conf
+sudo leafpad /etc/NetworkManager/NetworkManager.conf
 
 echo -e "\e[32mPreparing configuration successfully!\e[0m"
 
